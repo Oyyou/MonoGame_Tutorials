7ec2830f5af6ed658ae6a63222645a660d9bd38d
Niall Lewin <Lewin91@hotmail.com>
2018-04-26 11:28:49 +0100

    Uploaded "Tutorial003"

---
+++ MonoGame_Tutorials/Tutorial003/Game1.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Tutorial003
+{
+  /// <summary>
+  /// This is the main type for your game.
+  /// </summary>
+  public class Game1 : Game
+  {
+    GraphicsDeviceManager graphics;
+    SpriteBatch spriteBatch;
+
+    private Sprite _sprite1;
+    private Sprite _sprite2;
+
+    public Game1()
+    {
+      graphics = new GraphicsDeviceManager(this);
+      Content.RootDirectory = "Content";
+    }
+
+    /// <summary>
+    /// Allows the game to perform any initialization it needs to before starting to run.
+    /// This is where it can query for any required services and load any non-graphic
+    /// related content.  Calling base.Initialize will enumerate through any components
+    /// and initialize them as well.
+    /// </summary>
+    protected override void Initialize()
+    {
+      // TODO: Add your initialization logic here
+
+      base.Initialize();
+    }
+
+    /// <summary>
+    /// LoadContent will be called once per game and is the place to load
+    /// all of your content.
+    /// </summary>
+    protected override void LoadContent()
+    {
+      // Create a new SpriteBatch, which can be used to draw textures.
+      spriteBatch = new SpriteBatch(GraphicsDevice);
+
+      var texture = Content.Load<Texture2D>("Box");
+
+      _sprite1 = new Sprite(texture);
+      _sprite1.Position = new Vector2(100, 100);
+
+      _sprite2 = new Sprite(texture)
+      {
+        Position = new Vector2(200, 100),
+        Speed = 3f,
+      };
+    }
+
+    /// <summary>
+    /// UnloadContent will be called once per game and is the place to unload
+    /// game-specific content.
+    /// </summary>
+    protected override void UnloadContent()
+    {
+      // TODO: Unload any non ContentManager content here
+    }
+
+    /// <summary>
+    /// Allows the game to run logic such as updating the world,
+    /// checking for collisions, gathering input, and playing audio.
+    /// </summary>
+    /// <param name="gameTime">Provides a snapshot of timing values.</param>
+    protected override void Update(GameTime gameTime)
+    {
+      _sprite1.Update();
+      _sprite2.Update();
+
+      base.Update(gameTime);
+    }
+
+    /// <summary>
+    /// This is called when the game should draw itself.
+    /// </summary>
+    /// <param name="gameTime">Provides a snapshot of timing values.</param>
+    protected override void Draw(GameTime gameTime)
+    {
+      GraphicsDevice.Clear(Color.CornflowerBlue);
+
+      spriteBatch.Begin();
+      
+      _sprite1.Draw(spriteBatch);
+      _sprite2.Draw(spriteBatch);
+
+      spriteBatch.End();
+
+      base.Draw(gameTime);
+    }
+  }
+}
---
+++ MonoGame_Tutorials/Tutorial003/Sprite.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial003
+{
+  public class Sprite
+  {
+    private Texture2D _texture;
+
+    public Vector2 Position;
+
+    public float Speed = 2f;
+
+    public Sprite(Texture2D texture)
+    {
+      _texture = texture;
+    }
+
+    public void Update()
+    {
+      if (Keyboard.GetState().IsKeyDown(Keys.W))
+      {
+        Position.Y -= Speed;
+      }
+      if (Keyboard.GetState().IsKeyDown(Keys.S))
+      {
+        Position.Y += Speed;
+      }
+      if (Keyboard.GetState().IsKeyDown(Keys.A))
+      {
+        Position.X -= Speed;
+      }
+      if (Keyboard.GetState().IsKeyDown(Keys.D))
+      {
+        Position.X += Speed;
+      }
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+      spriteBatch.Draw(_texture, Position, Color.White);
+    }
+  }
+}
