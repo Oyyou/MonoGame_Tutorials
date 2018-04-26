ed8ee5278ed8bcd9d136c2abe8592f0443164e40
Niall Lewin <Lewin91@hotmail.com>
2018-04-26 11:48:53 +0100

    Uploaded "Tutorial004"

---
+++ MonoGame_Tutorials/Tutorial004/Input.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial004
+{
+  public class Input
+  {
+    public Keys Left { get; set; }
+    public Keys Right { get; set; }
+    public Keys Up { get; set; }
+    public Keys Down { get; set; }
+  }
+}
---
+++ MonoGame_Tutorials/Tutorial004/Sprite.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial004
+{
+  public class Sprite
+  {
+    private Texture2D _texture;
+
+    public Vector2 Position;
+
+    public float Speed = 2f;
+
+    public Input Input;
+
+    public Sprite(Texture2D texture)
+    {
+      _texture = texture;
+    }
+
+    public void Update()
+    {
+      Move();
+    }
+
+    public void Move()
+    {
+      if (Input == null)
+        return;
+
+      if (Keyboard.GetState().IsKeyDown(Input.Left))
+      {
+        Position.X -= Speed;
+      }
+
+      if (Keyboard.GetState().IsKeyDown(Input.Right))
+      {
+        Position.X += Speed;
+      }
+
+      if (Keyboard.GetState().IsKeyDown(Input.Up))
+      {
+        Position.Y -= Speed;
+      }
+
+      if (Keyboard.GetState().IsKeyDown(Input.Down))
+      {
+        Position.Y += Speed;
+      }
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+      spriteBatch.Draw(_texture, Position, Color.White);
+    }
+  }
+}
