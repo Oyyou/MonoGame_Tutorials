5a5a49dceb28f5b8617a4b97a35a58d52a4a8837
Niall Lewin <Lewin91@hotmail.com>
2018-04-26 12:47:17 +0100

    Uploaded "Tutorial005"

---
+++ MonoGame_Tutorials/Tutorial005/Game1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Tutorial005
+{
+  /// <summary>
+  /// This is the main type for your game.
+  /// </summary>
+  public class Game1 : Game
+  {
+    GraphicsDeviceManager graphics;
+    SpriteBatch spriteBatch;
+
+    private Sprite _sprite;
+
+    public Game1()
+    {
+      graphics = new GraphicsDeviceManager(this);
+      Content.RootDirectory = "Content";
+    }
+
+    /// <summary>
+    /// Allows the game to perform any initialization it needs to before starting to run.
+    /// This is where it can query for any required services and load any non-graphic
+    /// related content.  Calling base.Initialize will enumerate through any components
+    /// and initialize them as well.
+    /// </summary>
+    protected override void Initialize()
+    {
+      // TODO: Add your initialization logic here
+
+      base.Initialize();
+    }
+
+    /// <summary>
+    /// LoadContent will be called once per game and is the place to load
+    /// all of your content.
+    /// </summary>
+    protected override void LoadContent()
+    {
+      // Create a new SpriteBatch, which can be used to draw textures.
+      spriteBatch = new SpriteBatch(GraphicsDevice);
+
+      var texture = Content.Load<Texture2D>("Sprite");
+
+      _sprite = new Sprite(texture)
+      {
+        Position = new Vector2(100, 100),
+        Origin = new Vector2(texture.Width / 2, texture.Height - 25),
+      };
+    }
+
+    /// <summary>
+    /// UnloadContent will be called once per game and is the place to unload
+    /// game-specific content.
+    /// </summary>
+    protected override void UnloadContent()
+    {
+      // TODO: Unload any non ContentManager content here
+    }
+
+    /// <summary>
+    /// Allows the game to run logic such as updating the world,
+    /// checking for collisions, gathering input, and playing audio.
+    /// </summary>
+    /// <param name="gameTime">Provides a snapshot of timing values.</param>
+    protected override void Update(GameTime gameTime)
+    {
+      _sprite.Update();
+
+      base.Update(gameTime);
+    }
+
+    /// <summary>
+    /// This is called when the game should draw itself.
+    /// </summary>
+    /// <param name="gameTime">Provides a snapshot of timing values.</param>
+    protected override void Draw(GameTime gameTime)
+    {
+      GraphicsDevice.Clear(Color.CornflowerBlue);
+
+      spriteBatch.Begin();
+
+      _sprite.Draw(spriteBatch);
+
+      spriteBatch.End();
+
+      base.Draw(gameTime);
+    }
+  }
+}
---
+++ MonoGame_Tutorials/Tutorial005/Sprite.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial005
+{
+  public class Sprite
+  {
+    private Texture2D _texture;
+
+    private float _rotation;
+
+    public Vector2 Position;
+
+    /// <summary>
+    /// The point we rotate on
+    /// </summary>
+    public Vector2 Origin;
+
+    /// <summary>
+    /// The speed of the rotation
+    /// </summary>
+    public float RotationVelocity = 3f;
+
+    /// <summary>
+    /// The speed of moving forward
+    /// </summary>
+    public float LinearVelocity = 4f;
+
+    public Sprite(Texture2D texture)
+    {
+      _texture = texture;
+    }
+
+    public void Update()
+    {
+      if (Keyboard.GetState().IsKeyDown(Keys.A))
+        _rotation -= MathHelper.ToRadians(RotationVelocity);
+      else if (Keyboard.GetState().IsKeyDown(Keys.D))
+        _rotation += MathHelper.ToRadians(RotationVelocity);
+
+      var direction = new Vector2((float)Math.Cos(MathHelper.ToRadians(90) - _rotation), -(float)Math.Sin(MathHelper.ToRadians(90) - _rotation));
+
+      if (Keyboard.GetState().IsKeyDown(Keys.W))
+        Position += direction * LinearVelocity;
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+      spriteBatch.Draw(_texture, Position, null, Color.White, _rotation, Origin, 1, SpriteEffects.None, 0f);
+    }
+  }
+}
