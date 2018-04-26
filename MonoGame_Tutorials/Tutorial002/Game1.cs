c6922a471b9f03920f993520211c273dc37705ed
Niall Lewin <Lewin91@hotmail.com>
2018-04-26 11:08:56 +0100

    Uploaded "Tutorial002"

---
+++ MonoGame_Tutorials/Tutorial002/Game1.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Tutorial002
+{
+  /// <summary>
+  /// This is the main type for your game.
+  /// </summary>
+  public class Game1 : Game
+  {
+    GraphicsDeviceManager graphics;
+    SpriteBatch spriteBatch;
+
+    private Texture2D _texture;
+    private Vector2 _position;
+
+    public Game1()
+    {
+      graphics = new GraphicsDeviceManager(this);
+      Content.RootDirectory = "Content";
+    }
+
+    /// <summary>
+    /// Allows the game to perform any initialization it needs to before starting to run.
+    /// This is where it can query for any required services and load any non-graphic
+    /// related content.  Calling base.Initialize will enumerate through any components
+    /// and initialize them as well.
+    /// </summary>
+    protected override void Initialize()
+    {
+      // TODO: Add your initialization logic here
+
+      base.Initialize();
+    }
+
+    /// <summary>
+    /// LoadContent will be called once per game and is the place to load
+    /// all of your content.
+    /// </summary>
+    protected override void LoadContent()
+    {
+      // Create a new SpriteBatch, which can be used to draw textures.
+      spriteBatch = new SpriteBatch(GraphicsDevice);
+
+      _texture = Content.Load<Texture2D>("Box");
+      _position = new Vector2(0, 0);
+    }
+
+    /// <summary>
+    /// UnloadContent will be called once per game and is the place to unload
+    /// game-specific content.
+    /// </summary>
+    protected override void UnloadContent()
+    {
+      // TODO: Unload any non ContentManager content here
+    }
+
+    /// <summary>
+    /// Allows the game to run logic such as updating the world,
+    /// checking for collisions, gathering input, and playing audio.
+    /// </summary>
+    /// <param name="gameTime">Provides a snapshot of timing values.</param>
+    protected override void Update(GameTime gameTime)
+    {
+      if (Keyboard.GetState().IsKeyDown(Keys.W))
+      {
+        _position.Y -= 1;
+      }
+      if (Keyboard.GetState().IsKeyDown(Keys.S))
+      {
+        _position.Y += 1;
+      }
+      if (Keyboard.GetState().IsKeyDown(Keys.A))
+      {
+        _position.X -= 1;
+      }
+      if (Keyboard.GetState().IsKeyDown(Keys.D))
+      {
+        _position.X += 1;
+      }
+
+      base.Update(gameTime);
+    }
+
+    /// <summary>
+    /// This is called when the game should draw itself.
+    /// </summary>
+    /// <param name="gameTime">Provides a snapshot of timing values.</param>
+    protected override void Draw(GameTime gameTime)
+    {
+      GraphicsDevice.Clear(Color.CornflowerBlue);
+
+      spriteBatch.Begin();
+
+      spriteBatch.Draw(_texture, _position, Color.White);
+
+      spriteBatch.End();
+
+      base.Draw(gameTime);
+    }
+  }
+}
