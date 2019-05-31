4f5b38982ce26c8d8eaf9af1fa45325fec13e110
Niall Lewin <Lewin91@hotmail.com>
2019-05-31 11:21:34 +0100

    Added a "Utilities" project
    Currently the project deletes the bin and obj folders

---
+++ MonoGame_Tutorials/Utilities/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      DeleteSillyFolders();
+    }
+
+    private static void DeleteSillyFolders()
+    {
+      var test = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
+
+      CheckAndDelete(test);
+    }
+
+    private static void CheckAndDelete(string path)
+    {
+      var foldersToDelete = new List<string>()
+      {
+        "Bin",
+        "Obj",
+      };
+
+      var directories = Directory.GetDirectories(path);
+
+      foreach (var directory in directories)
+      {
+        var directoryName = new DirectoryInfo(directory).Name;
+
+        if (foldersToDelete.Any(c => string.Equals(c, directoryName, StringComparison.CurrentCultureIgnoreCase)))
+        {
+          try
+          {
+            Directory.Delete(directory, true);
+          }
+          catch (UnauthorizedAccessException)
+          {
+            Console.WriteLine("This is expected");
+          }
+          catch (Exception e)
+          {
+            throw new Exception(e.ToString());
+          }
+
+          continue;
+        }
+
+        CheckAndDelete(directory);
+      }
+    }
+  }
+}
